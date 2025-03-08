9a741e1d6b18bcff4d19ebf3d6a75f133570db18
mittim88 <timeschert@yahoo.de>
2025-03-08 19:48:13 +0100

    added Data

---
+++ dev/updateTest.lua
@@ -0,0 +1,24 @@
+-- @description Testing Update function
+-- @reaScript Name update Test Script
+-- @author mittim88
+-- @version 4.0.0
+-- @provides 
+--      /dev/*.lua
+--      /dev/*.pdf
+--   /TC_Helper/data/pdf/*.pdf
+--   /TC_Helper/data/keyMap/*.ReaperKeyMap
+--   /TC_Helper/data/images/*.png
+--   /TC_Helper/data/logo/*.png
+--   /TC_Helper/data/projectTemplate/*.RPP
+
+
+
+
+
+
+function refreshAndBrowseTCHelper()
+    reaper.ReaPack_BrowsePackages('updateTest')
+    reaper.ReaPack_ProcessQueue(true)
+
+end
+refreshAndBrowseTCHelper()
