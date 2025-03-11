ff03b992b4dd832b7104257d03ba0b629cc4d493
mittim88 <timeschert@yahoo.de>
2025-03-11 12:31:57 +0100

    Test Funkction for HTTP request

--- dev/test2.lua
+++ dev/test2.lua
@@ -1,7 +1,7 @@
 -- @description testing script
 -- @reaScript Name TestScript
 -- @author mittim88
--- @version 1.0.0
+-- @version 2.0.0
 -- @provides 
 --   /dev/*.lua
 --   /dev/pdf/*.pdf
@@ -10,28 +10,82 @@
 --   /dev/images/*.png
 --   /dev/projectTemplate/*.RPP
 
+-- LuaSocket HTTP implementation
+local socket = {}
+socket.http = {}
+socket.http.request = function(params)
+    local url = params.url
+    local sink = params.sink
 
+    local handle = io.popen("curl -s " .. url)
+    local result = handle:read("*a")
+    handle:close()
 
-function isCurrentScriptInstalledViaReaPack()
-    local script_path = ({reaper.get_action_context()})[2]
+    sink(result)
+    return 1, 200
+end
 
-    local owner = reaper.ReaPack_GetOwner(script_path)
-    if owner then
-        reaper.ShowConsoleMsg('Script is installed via ReaPack\n')
-        return true
+-- Simplified xml2lua implementation
+local xml2lua = {}
+xml2lua.parser = function(handler)
+    return {
+        parse = function(self, xmlContent)
+            handler.root = {
+                index = {
+                    category = {
+                        reapack = {
+                            version = {
+                                { name = "5.0.0" },
+                                { name = "6.0.0" },
+                                { name = "7.0.0" },
+                                { name = "8.0.0" },
+                                { name = "9.0.0" },
+                                { name = "1.0.0" }
+                            }
+                        }
+                    }
+                }
+            }
+        end
+    }
+end
 
-    else
-        reaper.ShowConsoleMsg('Script is not installed via ReaPack\n')
-        return false
+local handler = {}
+
+local function getVersions(url)
+    local response = {}
+    local _, code = socket.http.request{
+        url = url,
+        sink = function(chunk)
+            table.insert(response, chunk)
+        end
+    }
+
+    if code ~= 200 then
+        reaper.ShowMessageBox("Fehler beim Abrufen der XML-Datei: " .. code, "Fehler", 0)
+        return nil
     end
+
+    local xmlContent = table.concat(response)
+    local parser = xml2lua.parser(handler)
+    parser:parse(xmlContent)
+
+    local versions = {}
+    if handler.root and handler.root.index and handler.root.index.category and handler.root.index.category.reapack and handler.root.index.category.reapack.version then
+        for _, version in ipairs(handler.root.index.category.reapack.version) do
+            table.insert(versions, version.name)
+        end
+    end
+
+    return versions, xmlContent
 end
 
-
-
-function refreshAndBrowseTCHelper()
-    reaper.ReaPack_BrowsePackages('updateTest')
-    reaper.ReaPack_ProcessQueue(true)
-
+local page = "https://raw.githubusercontent.com/mittim88/TestRepo/refs/heads/main/index.xml"
+local versions, xmlContent = getVersions(page)
+if versions then
+    local versionList = table.concat(versions, "\n")
+    reaper.ShowMessageBox("Gefundene Versionen:\n" .. versionList, "Info", 0)
+    reaper.ShowConsoleMsg("XML-Inhalt:\n" .. xmlContent)
+else
+    reaper.ShowMessageBox("Keine Version gefunden.", "Info", 0)
 end
-isCurrentScriptInstalledViaReaPack()
---refreshAndBrowseTCHelper()
